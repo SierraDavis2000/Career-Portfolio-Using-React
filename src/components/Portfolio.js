91cbab3de4dad5e32599b7c7514e08ea62050220
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-07-10 20:46:52 -0400

    project and portfolio componets created

---
+++ src/components/Portfolio.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Project from './Project';
+
+function Portfolio() {
+	const projects = [
+		{
+			name: 'Home Garden Manager',
+			description:
+				'A site that allows plant enthusiasts to easily find care instructions for plants they own, as well as contribute to the plant database, and create logs to track their plant watering activity.',
+			image: 'garden-image.png',
+			github: 'https://github.com/SierraDavis2000/Home-Garden-Manager',
+			deployed: 'https://warm-harbor-34559.herokuapp.com/',
+		},
+		{
+			name: 'Weather Dashboard',
+			description:
+				'A weather dashboard that shows current weather and five-day weather forecast.',
+			image: 'weather-image.png',
+			github: 'https://github.com/SierraDavis2000/Weather-Dashboard',
+			deployed: 'https://sierradavis2000.github.io/Weather-Dashboard/',
+		},
+		{
+			name: 'Password Generator',
+			description:
+				'This is an application that allows you to generate a password based on user criteria.',
+			image: 'password-image.png',
+			github: 'https://github.com/SierraDavis2000/Password-Generator',
+			deployed: 'https://sierradavis2000.github.io/Password-Generator/',
+		},
+		{
+			name: 'Note Taker',
+			description:
+				'This application allows the user to enter and save notes.',
+			image: 'notetaker-image.png',
+			github: 'https://github.com/SierraDavis2000/Note-Taker-Application-Featuring-Express.js',
+			deployed: 'https://whispering-woodland-26598.herokuapp.com/',
+		},
+	];
+
+	return (
+		<section>
+			<div className="center">
+				<h1 className="page-header">Sierra's Portfolio</h1>
+			</div>
+			<div>
+				<ul className="flex-row mobile-row">
+					<li className="padding">
+						<Project projects={projects[0]}></Project>
+					</li>
+					<li className="padding">
+						<Project projects={projects[1]}></Project>
+					</li>
+				</ul>
+				<ul className="flex-row mobile-row">
+					<li className="padding">
+						<Project projects={projects[2]}></Project>
+					</li>
+					<li className="padding">
+						<Project projects={projects[3]}></Project>
+					</li>
+				</ul>
+			</div>
+		</section>
+	);
+}
+
+export default Portfolio;
---
+++ src/components/Project.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+
+function Project(props) {
+	
+	const viewProject = useState(props)[0].projects;
+
+	const name = viewProject.name;
+	const description = viewProject.description;
+	const image = viewProject.image;
+	const deployLink = viewProject.deployed;
+	const gitRepo = viewProject.github;
+
+	
+
+
+	return (
+		<Card style={{ width: "18rem" }}>
+			<Card.Img
+				variant="top"
+                //add image 
+				src={require(`../assets/images/${image}`)}
+				className="card-image"
+			/>
+			<div className="center">
+				<Card.Body>
+					<Card.Title className="card-title">{name}</Card.Title>
+					<Card.Text className="card-text">{description}</Card.Text>
+					<Card.Link href={deployLink} target="_blank" className="card-link">
+						{name} Link to Deployed App
+					</Card.Link>
+					<br></br>
+					<Card.Link href={gitRepo} target="_blank" className="card-link">
+						{name} Link to Project Github
+					</Card.Link>
+				</Card.Body>
+			</div>
+		</Card>
+	);
+}
+
+export default Project;
